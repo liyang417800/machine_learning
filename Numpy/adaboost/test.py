0202b6c364c6b88fdfd102fd6efef4adbf62d196
liyang417800 <317616515@qq.com>
2017-08-11 18:09:38 +0800

    svm

--- Numpy/adaboost/test.py
+++ Numpy/adaboost/test.py
@@ -1,28 +1,10 @@
 from numpy import *
 
-def loadSimpData():
-    datMat = matrix([[1.,2.1],
-                     [2.,1.1],
-                     [1.3,1.],
-                     [1.,1.],
-                     [2.,1.]])
-    classLabels = [1.0,1.0,-1.0,-1.0,-1.0]
+a=mat('1 2 3; 4 5 3')
 
-    return datMat,classLabels
+print a
+print a.T
+print a.I
 
-
-def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
-    # dimen 0; threshval 0.9; threshIneq ['lt', 'gt']
-    retArray = ones((shape(dataMatrix)[0],1))
-
-    if threshIneq == 'It':
-        retArray[dataMatrix[:,dimen]<=threshVal] = -1.0
-        print retArray
-    else:
-        retArray[dataMatrix[:,dimen]>threshVal] = -1.0
-        print retArray
-    return retArray
-
-if __name__=='__main__':
-    datMat,classLabels = loadSimpData()
-    print stumpClassify(datMat,0,0.9,'It')
+print a*a.I
+# print (a*a.T).I
