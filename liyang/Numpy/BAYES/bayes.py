271d0548b2fc045159ef989d5aac74548919843b
liyang417800 <317616515@qq.com>
2017-09-04 17:36:05 +0800

    svm

--- liyang/Numpy/BAYES/bayes.py
+++ liyang/Numpy/BAYES/bayes.py
@@ -12,12 +12,14 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1]         #1代表侮辱性文字,0代表正常言语
     return postingList,classVec
 
+#合并所有的字符并且去重
 def createVocabList(dataSet):
-    vocabSet = set([])  #create empty set
+    vocabSet = set([])  #创造空集
     for document in dataSet:
-        vocabSet = vocabSet | set(document) #union of the two sets
+        vocabSet = vocabSet | set(document) #取出两个集合的并集,对于重复的只取一次
     return list(vocabSet)
 
+#判断一个list中所有字符在整个去重集合中是否出现(词向量)
 def setOfWords2Vec(vocabList,inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
@@ -35,10 +37,11 @@ def bagOfWord2VecMN(vocabList,inputSet):
     return returnVec
 
 
+#训练函数
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)  #所有list的数量,list长度为6
     numWords = len(trainMatrix[0])  #一个list的长度,长度为32
-    pAbusive = sum(trainCategory) / float(numTrainDocs)  #??
+    pAbusive = sum(trainCategory) / float(numTrainDocs)  # 3/6
     # p0Num = zeros(numWords)
     # p1Num = zeros(numWords)
 
@@ -47,12 +50,11 @@ def trainNB0(trainMatrix,trainCategory):
 
     # p0Denom = 0.0; p1Denom = 0.0
     p0Denom = 2.0; p1Denom = 2.0
-    for i in range(numTrainDocs):  #从0开始
+    for i in range(numTrainDocs):  #从0开始,计算每个类目的词条的计数总和
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
             p1Denom += sum(trainMatrix[i])
         else:
-
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
 
@@ -80,16 +82,16 @@ def testingNB():
         trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
     p0V,p1V,pAb = trainNB0(trainMat,listClasses)
 
+
     testEntry = ['love','my','dalmation']
 
     thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
     print testEntry,'classified as :',classifyNB(thisDoc,p0V,p1V,pAb)
 
-    testEntry = ['stupid','garbage']
+    testEntry = ['aaa','bbb']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
 
     print testEntry,'classified as :',classifyNB(thisDoc,p0V,p1V,pAb)
-
 
 def textParse(bigString):
     import re
@@ -97,9 +99,7 @@ def textParse(bigString):
     return [tok.lower() for tok in listOfTokens if len(tok) > 0]
 
 def spamTest():
-    docList=[]
-    classList=[]
-    fullText=[]
+    docList=[];classList=[];fullText=[]
     for i in range(1,26):
         wordList = textParse(open('/Users/yangli/Downloads/machinelearninginaction/Ch04/email/spam/%d.txt' % i).read())
         docList.append(wordList)
@@ -111,7 +111,6 @@ def spamTest():
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
-
 
     vocabList = createVocabList(docList)
     trainingSet = range(50); testSet=[]
@@ -143,7 +142,18 @@ def spamTest():
 
 if __name__=='__main__':
     listOPosts,listClasses = loadDataSet()
-    myVocabList = createVocabList(listOPosts) #去重所有的单词
-    # testingNB()
-    spamTest()
-    spamTest()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    # print trainMat
+    # print len(trainMat)
+    # print zeros(32)
+    # print sum(listClasses)
+    p0V,p1V,pAb = trainNB0(trainMat,listClasses)
+    # print p0V,p1V,pAb
+
+    testingNB()
+    # spamTest()
+    # spamTest()
+
