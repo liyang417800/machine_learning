b8336accb3aefd341b199927751726411e77b7d4
Administrator <317616515@qq.com>
2018-01-01 15:46:10 +0800

    Default

--- liyang/Numpy/LOGISTIC_FORECAST/regression.py
+++ liyang/Numpy/LOGISTIC_FORECAST/regression.py
@@ -1,9 +1,9 @@
 #coding:utf-8
-
 from numpy import *
+import numpy as np
 
 import matplotlib.pyplot as plt
-
+np.seterr(divide='ignore', invalid='ignore')
 def loadDataSet(fileName):      #打开文件
     numFeat = len(open(fileName).readline().split('\t')) - 1 #得到特征的数量
     dataMat = []; labelMat = []
@@ -101,13 +101,14 @@ def stageWise(xArr,yArr,eps=0.01,numIt=100):
     yMat = yMat - yMean     #can also regularize ys but will get smaller coef
     xMat = regularize(xMat)
     m,n=shape(xMat)
+    print m,n
     returnMat = zeros((numIt,n)) #testing code remove
     ws = zeros((n,1))
     wsTest = ws.copy()
     wsMax = ws.copy()
     for i in range(numIt):
         print ws.T
-        lowestError = inf;
+        lowestError = inf;  #正无穷大
         for j in range(n):
             for sign in [-1,1]:
                 wsTest = ws.copy()
@@ -122,17 +123,91 @@ def stageWise(xArr,yArr,eps=0.01,numIt=100):
     return returnMat
 
 
+from time import sleep
+import json
+import urllib2
+
+
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+    sleep(10)
+    myAPIstr = 'AIzaSyD2cR2KFyx12hXu6PFU-wrWot3NXvko8vY'
+    searchURL = 'https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json' % (
+    myAPIstr, setNum)
+    pg = urllib2.urlopen(searchURL)
+    retDict = json.loads(pg.read())
+    for i in range(len(retDict['items'])):
+        try:
+            currItem = retDict['items'][i]
+            if currItem['product']['condition'] == 'new':
+                newFlag = 1
+            else:
+                newFlag = 0
+            listOfInv = currItem['product']['inventories']
+            for item in listOfInv:
+                sellingPrice = item['price']
+                if sellingPrice > origPrc * 0.5:
+                    print "%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrice)
+                    retX.append([yr, numPce, newFlag, origPrc])
+                    retY.append(sellingPrice)
+        except:
+            print 'problem with item %d' % i
+
+
+def setDataCollect(retX, retY):
+    searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+    searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+    searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+    searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+    searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+    searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
+
+def crossValidation(xArr,yArr,numVal=10):
+    m = len(yArr)
+    indexList = range(m)
+    errorMat = zeros((numVal,30))#create error mat 30columns numVal rows
+    for i in range(numVal):
+        trainX=[]; trainY=[]
+        testX = []; testY = []
+        random.shuffle(indexList)
+        for j in range(m):#create training set based on first 90% of values in indexList
+            if j < m*0.9:
+                trainX.append(xArr[indexList[j]])
+                trainY.append(yArr[indexList[j]])
+            else:
+                testX.append(xArr[indexList[j]])
+                testY.append(yArr[indexList[j]])
+        wMat = ridgeTest(trainX,trainY)    #get 30 weight vectors from ridge
+        for k in range(30):#loop over all of the ridge estimates
+            matTestX = mat(testX); matTrainX=mat(trainX)
+            meanTrain = mean(matTrainX,0)
+            varTrain = var(matTrainX,0)
+            matTestX = (matTestX-meanTrain)/varTrain #regularize test with training params
+            yEst = matTestX * mat(wMat[k,:]).T + mean(trainY)#test ridge results and store
+            errorMat[i,k]=rssError(yEst.T.A,array(testY))
+            #print errorMat[i,k]
+    meanErrors = mean(errorMat,0)#calc avg performance of the different ridge weight vectors
+    minMean = float(min(meanErrors))
+    bestWeights = wMat[nonzero(meanErrors==minMean)]
+    #can unregularize to get model
+    #when we regularized we wrote Xreg = (x-meanX)/var(x)
+    #we can now write in terms of x not Xreg:  x*w/var(x) - meanX/var(x) +meanY
+    xMat = mat(xArr); yMat=mat(yArr).T
+    meanX = mean(xMat,0); varX = var(xMat,0)
+    unReg = bestWeights/varX
+    print "the best model from Ridge Regression is:\n",unReg
+    print "with constant term: ",-1*sum(multiply(meanX,unReg)) + mean(yMat)
+
 if __name__=='__main__':
-    xArr,yArr = loadDataSet('ex0.txt')
+    # xArr,yArr = loadDataSet('ex0.txt')
     # print xArr[0:2] #[[1.0, 0.067732], [1.0, 0.42781]]
     # print mat(xArr[0:2])
     # print yArr[0:2] #[3.176513, 3.816464]
-    ws = standRegres(xArr,yArr) #[[ 3.00774324]
+    # ws = standRegres(xArr,yArr) #[[ 3.00774324]
                                 # [ 1.69532264]]
-    xMat = mat(xArr) #转换为矩阵
-    yMat = mat(yArr) #转换为矩阵
+    # xMat = mat(xArr) #转换为矩阵
+    # yMat = mat(yArr) #转换为矩阵
 
-    yHat = xMat*ws
+    # yHat = xMat*ws
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -148,17 +223,17 @@ if __name__=='__main__':
     # print yArr[0]
     # print lwlr(xArr[0],xArr,yArr,1.0)
     # print lwlr(xArr[0],xArr,yArr,0.001)
-    yHat =  lwlrTest(xArr,xArr,yArr,0.01)
+    # yHat =  lwlrTest(xArr,xArr,yArr,0.01)
 
-    srtInd = xMat[:,1].argsort(0) #按照所选列进行升序排列
+    # srtInd = xMat[:,1].argsort(0) #按照所选列进行升序排列
     # print srtInd
-    xSort = xMat[srtInd][:,0,:]
+    # xSort = xMat[srtInd][:,0,:]
     # print xSort
     # ax.plot(xSort[:,1],yHat[srtInd])
     # ax.scatter(xMat[:,1].flatten().A[0], mat(yArr).T.flatten().A[0],s=2,c='red')
     # plt.show()
 
-    abX,abY = loadDataSet('abalone.txt')
+    # abX,abY = loadDataSet('abalone.txt')
     # yHatO1=lwlrTest(abX[0:99],abX[0:99],abY[0:99],0.1)
     # yHat1=lwlrTest(abX[0:99],abX[0:99],abY[0:99],1)
     # yHat10=lwlrTest(abX[0:99],abX[0:99],abY[0:99],10)
@@ -177,9 +252,11 @@ if __name__=='__main__':
     # ws = standRegres(abX[0:99],abY[0:99])
     # yHat = mat(abX[100:199])*ws
     # print rssError(abY[100:199],yHat.T.A)
-    ridgeWeights = ridgeTest(abX,abY)
-    ax.plot(ridgeWeights)
-    plt.show()
+    # ridgeWeights = ridgeTest(abX,abY)
+    # ax.plot(ridgeWeights)
+    # plt.show()
+    xArr, yArr = loadDataSet('abalone.txt')
+    stageWise(xArr,yArr,0.001,5000)
 
 
 
