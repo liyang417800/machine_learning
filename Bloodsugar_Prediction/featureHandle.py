cd269a9fb1efc4b0814a132b9630a8b84052b5ee
liyang417800 <317616515@qq.com>
2018-01-19 11:47:07 +0800

    预测血糖值

--- Bloodsugar_Prediction/featureHandle.py
+++ Bloodsugar_Prediction/featureHandle.py
@@ -18,7 +18,6 @@ sns.set_style('darkgrid')
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt  # Matlab-style plotting
-import seaborn as sns
 color = sns.color_palette()
 sns.set_style('darkgrid')
 import warnings
@@ -60,52 +59,52 @@ test = pd.read_csv(feature_save_path_t)
 
 
 #特征重要性
-from sklearn.cross_validation import cross_val_score, ShuffleSplit
-# from sklearn.datasets import load_boston
-from sklearn.ensemble import RandomForestRegressor
-
-def get_features_target(data):
-    data_array=pd.np.array(data)#传入dataframe，为了遍历，先转为array
-    features_list=[]
-    target_list=[]
-    for line in data_array:
-        temp_list=[]
-        for i in range(0,42):
-            if i == 1:
-                if line[i]=="男":
-                    line[i]=1
-                else:
-                    line[i]=0
-            if i ==3:
-                line[3] = line[3].replace('/','')
-            if i == 41 :
-                target_temp=(line[i])
-            else:
-                temp_list.append(line[i])
-        features_list.append(temp_list)
-        target_list.append(target_temp)
-    return features_list, target_list
-feature_save_path = "C:\Users\Administrator\machine_learning\Bloodsugar_Prediction\data\d_train_20180102.csv"  #将最终生成的特征存入该文件
-# feature_save_path = "./data/lable_pre.csv"  #将最终生成的特征存入该文件
-data = pd.read_csv(feature_save_path)
-
-data = data.fillna(0)
-
-feature,lable = get_features_target(data)
-names = list(data.columns)
-# print names
-feature = np.array(feature)
-# print feature.head()
-# print feature[:, 0:0+1]
-
-rf = RandomForestRegressor(n_estimators=20, max_depth=4)
-scores = []
-
-for i in range(feature.shape[1]):
-     score = cross_val_score(rf, feature[:, i:i+1], lable, scoring="r2",
-                              cv=ShuffleSplit(len(feature), 3, .3))
-     scores.append((round(np.mean(score), 3), names[i]))
-print sorted(scores, reverse=True)
+# from sklearn.cross_validation import cross_val_score, ShuffleSplit
+# # from sklearn.datasets import load_boston
+# from sklearn.ensemble import RandomForestRegressor
+#
+# def get_features_target(data):
+#     data_array=pd.np.array(data)#传入dataframe，为了遍历，先转为array
+#     features_list=[]
+#     target_list=[]
+#     for line in data_array:
+#         temp_list=[]
+#         for i in range(0,42):
+#             if i == 1:
+#                 if line[i]=="男":
+#                     line[i]=1
+#                 else:
+#                     line[i]=0
+#             if i ==3:
+#                 line[3] = line[3].replace('/','')
+#             if i == 41 :
+#                 target_temp=(line[i])
+#             else:
+#                 temp_list.append(line[i])
+#         features_list.append(temp_list)
+#         target_list.append(target_temp)
+#     return features_list, target_list
+# feature_save_path = "./data/d_train_20180102.csv"  #将最终生成的特征存入该文件
+# # feature_save_path = "./data/lable_pre.csv"  #将最终生成的特征存入该文件
+# data = pd.read_csv(feature_save_path)
+#
+# data = data.fillna(0)
+#
+# feature,lable = get_features_target(data)
+# names = list(data.columns)
+# # print names
+# feature = np.array(feature)
+# # print feature.head()
+# # print feature[:, 0:0+1]
+#
+# rf = RandomForestRegressor(n_estimators=20, max_depth=4)
+# scores = []
+#
+# for i in range(feature.shape[1]):
+#      score = cross_val_score(rf, feature[:, i:i+1], lable, scoring="r2",
+#                               cv=ShuffleSplit(len(feature), 3, .3))
+#      scores.append((round(np.mean(score), 3), names[i]))
+# print sorted(scores, reverse=True)
 
 
 
@@ -118,15 +117,17 @@ print sorted(scores, reverse=True)
 
 # print data['*天门冬氨酸氨基转换酶'].describe()
 
-#画图删除数据（离群值）
-#训练数据中可能存在其他离群值。然而，如果测试数据中也有异常值，除去所有这些可能会严重影响我们的模型。这就是为什么我们不把它们全部移除，而是设法使我们的一些模型对它们健壮。你可以参考这个笔记本的造型部分。
+# 画图删除数据（离群值）
+# 训练数据中可能存在其他离群值。然而，如果测试数据中也有异常值，除去所有这些可能会严重影响我们的模型。这就是为什么我们不把它们全部移除，而是设法使我们的一些模型对它们健壮。你可以参考这个笔记本的造型部分。
 # fig, ax = plt.subplots()
 # ax.scatter(x = data['*天门冬氨酸氨基转换酶'], y = data['血糖'])
 # plt.ylabel('血糖', fontsize=13)
 # plt.xlabel('天门冬氨酸氨基转换酶', fontsize=13)
+# plt.show()
 
-
-# train = data.drop(data[(data['血糖']>25)].index)
+#
+#
+# train = data.drop(data[(data['血糖']>20)].index)
 # fig, ax = plt.subplots()
 # ax.scatter(x = train['*天门冬氨酸氨基转换酶'], y = train['血糖'])
 # plt.ylabel('血糖', fontsize=13)
@@ -137,17 +138,17 @@ print sorted(scores, reverse=True)
 # mu = 5.63 and sigma = 1.54
 # sns.distplot(data['血糖'] , fit=norm);
 #
-# # Get the fitted parameters used by the function
+# # # Get the fitted parameters used by the function
 # (mu, sigma) = norm.fit(data['血糖'])
 # print( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
-#
-# #Now plot the distribution
+# #
+# # #Now plot the distribution
 # plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],
 #             loc='best')
 # plt.ylabel('Frequency')
 # plt.title('血糖 distribution')
-#
-# #Get also the QQ-plot
+# #
+# # #Get also the QQ-plot
 # fig = plt.figure()
 # res = stats.probplot(data['血糖'], plot=plt)
 # plt.show()
@@ -181,12 +182,12 @@ print sorted(scores, reverse=True)
 # all_data = pd.concat((data, test)).reset_index(drop=True)
 # all_data.drop(['血糖'], axis=1, inplace=True)
 # print("all_data size is : {}".format(all_data.shape)) #all_data size is : (6642, 41)
-#
+# #
 # all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
 # all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
 # missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
 # print missing_data.head(20)
-#
+# #
 # f, ax = plt.subplots(figsize=(15, 12))
 # plt.xticks(rotation='90')
 # sns.barplot(x=all_data_na.index, y=all_data_na)
@@ -196,7 +197,7 @@ print sorted(scores, reverse=True)
 # plt.show()
 
 # Data Correlation 数据相关性判断
-#Correlation map to see how features are correlated with SalePrice
+# Correlation map to see how features are correlated with SalePrice
 # corrmat = data.corr()
 # plt.subplots(figsize=(12,9))
 # sns.heatmap(corrmat, vmax=0.9, square=True)
@@ -205,20 +206,20 @@ print sorted(scores, reverse=True)
 #Imputing missing values
 #0，均值，中位数等
 #Adding one more important feature
-#需要查看官方文档，找到现有特征的联系，进行新特征的衍生
+# 需要查看官方文档，找到现有特征的联系，进行新特征的衍生
 # all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
 
 # numeric_feats = data.dtypes[data.dtypes != "object"].index
-#
-# # Check the skew of all numerical features
+# #
+# # # Check the skew of all numerical features
 # skewed_feats = data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
 # print("\nSkew in numerical features: \n")
 # skewness = pd.DataFrame({'Skew' :skewed_feats})
 # print skewness.head(10)
-#
+# #
 # skewness = skewness[abs(skewness) > 0.75]
 # print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
-#
+# #
 # from scipy.special import boxcox1p
 # skewed_features = skewness.index
 # lam = 0.15
@@ -253,41 +254,6 @@ print sorted(scores, reverse=True)
 
 
 
-#自定义列向量插值函数
-# def ploy(s,n,k=3):
-#     y=s[list(range(n-k,n))+list(range(n+1,n+1+k))]#取数
-#     y=y[y.notnull()]
-#     return lagrange(y.index,list(y))(n)
-# for i in data.columns:
-#     for j in range(len(data)):
-#         if(data[i].isnull())[j]:
-#             data[i][j]=ploy(data[i],j)
-# data.to_csv('./data/lable_pre.csv',index=False)
-
-
-
-# with open('./data/lable_pre.csv', "wb") as csvFile:
-#     csvWriter = csv.writer(csvFile)
-#     for i in range(len(data)):
-#         print data[i]
-#         csvWriter.writerow([data[i]])
-#     csvFile.close
-
-
-
-# data.to_excel('./data/1.xls')
-
-
-
-# data=DataFrame(np.random.randn(1000,4))
-# print(data.describe())
-#
-# print("\n....找出某一列中绝对值大小超过3的项...\n")
-# col=data[3]
-# print(col[np.abs(col) > 3] )
-#
-# print("\n....找出全部绝对值超过3的值的行...\n")
-# print(col[(np.abs(data) > 3).any(1)] )
 
 
 
@@ -295,13 +261,8 @@ print sorted(scores, reverse=True)
 
 
 
-
-
-
-
+#---------------------------------------------------
 # 只留下需要处理的列
-
-
 # print(df)
 # column = df.columns.values
 #
